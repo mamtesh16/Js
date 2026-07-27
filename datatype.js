e6583e7d979934c891bc325d605474aa3c9f9731
mamtesh kumar <mamteshk8612gmail.com>
2026-07-27 17:04:07 +0530

    conversion

---
+++ datatype.js
@@ -0,0 +1,7 @@
+ const accountId = 1235
+ let accountName="mamtesh"
+ let accountEmail = "mamtesh@gamil.com"
+ console.table([accountId,accountEmail,accountId])
+ console.log(typeof "mamtesh kumar");
+ 
+ 
