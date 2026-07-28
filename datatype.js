be858dbd1516ec93332b1dbf83ec8db9df07b442
mamtesh kumar <mamteshk8612gmail.com>
2026-07-29 01:28:45 +0530

    typeConverision

--- datatype.js
+++ datatype.js
@@ -1,7 +1,29 @@
- const accountId = 1235
- let accountName="mamtesh"
- let accountEmail = "mamtesh@gamil.com"
- console.table([accountId,accountEmail,accountId])
- console.log(typeof "mamtesh kumar");
+//  const accountId = 1235
+//  let accountName="mamtesh"
+//  let accountEmail = "mamtesh@gamil.com"
+//  console.table([accountId,accountEmail,accountId])
+//  console.log(typeof "mamtesh kumar");
  
  
+// alert("3+3")
+// let score = false // 0  
+// let score = undefined//nan
+// let score = true//op 1  
+// let score = null   //0
+// let score = "33abc"//nan
+// let score = "mamtesh"
+// console.log(typeof score)
+// type conversion
+// let valueInN = Number(score)
+// console.log(typeof valueInN)
+// console .log(valueInN);//not a number it means "33abc"Not a Number hai
+// 1=> true; 0=>false
+// let is = "mamtesh" // true
+// let is = "" //false
+// let m = Boolean(is)
+// console.log(m)
+let num = 16
+let str =  String(num)
+console.log (str)
+console.log(typeof str)//check that number is 
+// convert into a string
