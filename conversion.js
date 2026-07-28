a0e4c425eace1f8af6576d10c2650790ce1b0ca8
mamtesh kumar <mamteshk8612gmail.com>
2026-07-29 02:26:21 +0530

    Stringconversin

---
+++ conversion.js
@@ -0,0 +1,18 @@
+// let number = 3
+// let negValue  =-3
+// console.log(2+2);//4
+// console.log(2-2);//0
+// console.log(2*2);//4
+// console.log(2**3);//8
+// console.log(2/2);//1
+// let str1 = "mamtesh"
+// let str2 = " kumar"
+// let str3 = str1 +str2//mamtesh kumar
+// console.log(str3);
+//  console.log("1" + 3)// 13 if first letter is a string then whole value treat as a string
+//  console.log(2  + 3 + "3")// 53 if last letter is a string then first perform operation then 
+//   add 
+console.log(+true);// 1
+console.log(+"")//0
+
+
