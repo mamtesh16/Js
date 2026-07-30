0ac1238a488ba3f9ae0a3a8380dca8dbf7e5cec9
mamtesh kumar <mamteshk8612gmail.com>
2026-07-30 17:37:03 +0530

    Number & Math

---
+++ Nums & Math .js
@@ -0,0 +1,31 @@
+// const ma = new Number(100)// number
+//  const m = 100// it may be number or not
+// console.log(ma);
+// const mam = new String("mamtesh")
+// console.log(mam)
+ //console.log(m.toString().length)
+//console.log(typeof m)// op is number
+
+//console.log(m.toFixed(3))
+
+// const otherNumber = 12.7958
+
+//console.log(otherNumber.toPrecision(2));//2 means how many number before decimal
+
+// const hundreds =1000000000
+//console.log(hundreds.toLocaleString());//1,00,000....
+
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//********************Math******************
+console.log(Math);
+
+// console.log(Math.abs(-6))// -6=6,6=6
+// console.log(Math.round(4.4))//round
+// console.log(Math.floor(2.3))// less number  "2"
+// console.log(Math.ceil(2.1))//greater number "3"
+// console.log(Math.min(1,2,3,4,5,6))//use for find a minimum number
+// console.log(Math.max(2,4,3,6,5,99,8))//use for find a max number
+
+console.log(Math.random()) //0<=mr<1
+
