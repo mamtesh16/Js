a508b62d350002e8b0fa7f9402025c789e73bf79
mamtesh kumar <mamteshk8612gmail.com>
2026-07-31 16:20:05 +0530

    Array..

---
+++ array.js
@@ -0,0 +1,37 @@
+const mynum = [4,5,3,6,7,5]
+// console.log(mynum[2]);
+// console.log(mynum.length)
+
+// mynum.unshift(4)
+// mynum.shift()
+// mynum.push(10)
+// console.log(mynum)
+
+// console.log(mynum.includes(11))
+// console.log(mynum.indexOf(11))
+
+// array to string
+// const str = mynum.join()
+// console.log(str)
+// console.log(typeof str)// string
+
+//++++++++++++++++++++++++++++++++++++++++
+
+// const m = [4,5,6,7,8,9]
+// const n = [9,8,7,6,5,4]
+//  m.push(n)
+  //m.concat(n)
+ // console.log(m);
+//  const all = [...m,...n]//spread
+//  console.log(all);
+
+//  const mam = [6,[2,1,6,[4,2,1,2]]]
+//  const ab = mam.flat(Infinity)
+//  console.log(ab);
+
+ // for array
+ console.log(Array.isArray("mamtesh"))
+ // convert in array
+console.log(Array.from("mamtesh"))
+
+console.log(Array.from({Name:"mamtesh"}))//another case
