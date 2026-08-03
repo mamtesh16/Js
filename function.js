ddde49290585910c263c8c89e0d74ed04ca32d98
mamtesh kumar <mamteshk8612gmail.com>
2026-08-03 16:23:55 +0530

    Function

---
+++ function.js
@@ -0,0 +1,61 @@
+// function addnumber (num1 , num2){
+//     console.log(num1+num2)//return and print
+// }
+// console.log(addnumber(4,10));
+// // console.log(typeof addnumber)
+//  const result  =console.log(addnumber(4,"10"));//for string 
+//  console.log( "Result :",result)
+//***********************************/
+// function addnumber (num1 , num2){
+//     return num1 + num2
+//     // let result = num1+num2
+//     // return result;
+// }
+//  const value = addnumber(5,4)
+// console.log( " Result: ",value)
+// // console.log(typeof value) number
+
+// function loginUsermeassage(username){
+//     function loginUsermeassage(username="sam"){
+//     // if (username === undefined) {
+//          if (!username) {
+//       console.log("please enter a username")  
+//         return;
+//     }
+//     return `${username} just logged in`
+// }
+
+// //  console.log(loginUsermeassage("mamtesh"))
+//  console.log(loginUsermeassage())//undefind
+//++++++++++++++++++++++++++++++++++++++++++
+// function m(num) {
+//     return num % 2;
+// }
+//     //   console.log(m(7))
+// const n = m(7);
+
+// console.log("result :", n);
+//"""""""""""""""""""""""""""""""REST OPERATOR""""""""""""""""""""""""""""""""""""
+// function CalculatePrice(...num){
+//     return num
+// // rest operatro to do convert in array
+// }
+// m=CalculatePrice(100,200,300,400)
+// console.log(m);
+//----------------------------object------------------------------------
+// const user ={
+//     username :"mamtesh",
+//      dob :1612
+// }
+// function object(anyobject){
+//     console.log(`username is${anyobject.username} and dob is ${anyobject.dob}`)
+
+
+//     }
+//         object (user)
+//*********************************array**********************
+const array =[400,200,100,300]
+function secondevalue(second){
+    return array[2]
+}
+console.log(secondevalue(array))
