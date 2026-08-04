4abd68d632744330b60026494e79cd8d8f7008e4
mamtesh kumar <mamteshk8612gmail.com>
2026-08-05 00:45:46 +0530

    Immediately Invoked Function Expression(iife)

---
+++ iife.js
@@ -0,0 +1,14 @@
+////iife
+// (function one(num1, num2){
+//    console.log("mam")
+// })()
+//**************iife with arrow function
+// ( () =>{
+//    console.log(`mamtesh`)
+// } )();
+
+(function addTwo(num1,num2){
+  console.log(num1+num2)
+  
+})(4,5)
+
