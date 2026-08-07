35be0a086ce5f981ee0a273cd1b82616c132b579
mamtesh kumar <mamteshk8612gmail.com>
2026-08-07 15:29:11 +0530

    LOOP

---
+++ loop.js
@@ -0,0 +1,41 @@
+// for (let index = 0; index <10; index++) {
+//     const element = index;
+//     console.log( element + " " );
+    
+    
+// }
+
+// for (let i = 0; i <=5; i++) {
+//     for (let j = 0; j <=5; j++) {
+//    console.log( ` output  ${j}  and inner looop ${i}`)
+    
+// }
+   
+    
+// }
+// in array format
+//  let myarr = ["mamtesh", "kumar","gjhggj"]
+
+// for (let i= 0;  i < myarr.length; i ++) {
+//     const element = myarr [i];
+//     console.log(element);
+    
+    
+// }
+
+// break it means loop is exit
+// continue  did not print this value
+//***************************while loop************** */
+// let i = 
+// i = 0;
+// let arr=[4,5,7,8]
+// while (i< arr.length) {
+//     console.log(`value is ${arr[i]}`)
+//     i = i+1;
+// }
+//+++++++++++++++++++++++++++do while+++++++++++++++++++++
+let score = 1
+do {
+    console.log(` score is ${score}`)
+    score++
+} while (score <50);
