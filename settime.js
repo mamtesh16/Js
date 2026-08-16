67276bb99ad483cdfa820f9700e815e7a36a437d
mamtesh kumar <mamteshk8612gmail.com>
2026-08-17 00:46:51 +0530

    inital commit

---
+++ settime.js
@@ -0,0 +1,15 @@
+
+const sayMamtesh = function(){
+    console.log("Mamtesh");
+}
+
+const changeText = function(){
+    document.querySelector('h1').innerHTML = "best JS series"
+}
+
+const changeMe = setTimeout(changeText, 2000)
+
+document.querySelector('#stop').addEventListener('click', function(){
+    clearTimeout(changeMe)
+    console.log("STOPPED")
+})
